cd3932114a30f784e163f55536ef8370e1682a8d
nelson <peternelson063@gmail.com>
2023-12-29 12:17:38 +0100

    standard practice

---
+++ associate/standard-practice/global/s3/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate-locks-23672378t58237tr8"
+    key    = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-079db87dc4c10ac91"
+  instance_type          = "t2.micro"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstate-locks-23672378t58237tr8"
+  force_destroy = true
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Enable versioning so you can see the full revision history of your
+# state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
